f26899ca2999de26a40d815e59ec23619008217a
Harshilptl23 <iharshilpatel23@gmail.com>
2024-11-21 12:41:12 +0530

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text!="" && textBox2.Text !="")
+            {
+                int num1 = int.Parse(textBox1.Text);
+                int num2 = int.Parse(textBox2.Text);
+                int result = num1 + num2;
+                MessageBox.Show("Addition result : " + result.ToString());
+
+            }
+            else
+            {
+                MessageBox.Show("FILL BOTH THE FIELDS");
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                int num1 = int.Parse(textBox1.Text);
+                int num2 = int.Parse(textBox2.Text);
+                int result = num1 - num2;
+                MessageBox.Show("Subtraction result : " + result.ToString());
+
+            }
+            else
+            {
+                MessageBox.Show("FILL BOTH THE FIELDS");
+            }
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                int num1 = int.Parse(textBox1.Text);
+                int num2 = int.Parse(textBox2.Text);
+                int result = num1 * num2;
+                MessageBox.Show("Multiplication result : " + result.ToString());
+
+            }
+            else
+            {
+                MessageBox.Show("FILL BOTH THE FIELDS");
+            }
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if (textBox1.Text != "" && textBox2.Text != "")
+            {
+                int num1 = int.Parse(textBox1.Text);
+                int num2 = int.Parse(textBox2.Text);
+                int result = num1 / num2;
+                MessageBox.Show("Divison result : " + result.ToString());
+
+            }
+            else
+            {
+                MessageBox.Show("FILL BOTH THE FIELDS");
+            }
+        }
+    }
+}
